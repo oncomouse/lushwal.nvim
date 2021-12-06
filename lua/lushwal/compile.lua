bfe6b8e90f669990652b9c2c398bb9b3de8b409a
oncomouse <oncomouse@gmail.com>
2021-12-06 09:55:17 -0600

    Guard for configuration

--- lua/lushwal/compile.lua
+++ lua/lushwal/compile.lua
@@ -1,12 +1,14 @@
 -- luacheck: globals vim
+local config = require("lushwal.config")
 local function lushwal_compile()
-	vim.cmd([[packadd shipwright.nvim]])
-	if vim.fn.exists(":Shipwright") ~= 0 then
-		local xdg = require("lushwal.utils.xdg")
-		local cache_path = xdg("XDG_CACHE_HOME") .. "/lushwal"
-		vim.fn.mkdir(cache_path, "p")
-		local fp = io.open(cache_path .. "/shipwright_build.lua", "w")
-		fp:write([===[vim.cmd("packadd shipwright.nvim")
+	if config.compile_to_vimscript then
+		vim.cmd([[packadd shipwright.nvim]])
+		if vim.fn.exists(":Shipwright") ~= 0 then
+			local xdg = require("lushwal.utils.xdg")
+			local cache_path = xdg("XDG_CACHE_HOME") .. "/lushwal"
+			vim.fn.mkdir(cache_path, "p")
+			local fp = io.open(cache_path .. "/shipwright_build.lua", "w")
+			fp:write([===[vim.cmd("packadd shipwright.nvim")
 vim.cmd("packadd lush.nvim")
 local xdg = require("lushwal.utils.xdg")
 local colorscheme = require("lushwal")
@@ -33,12 +35,13 @@ lushwright.to_vimscript,
 { overwrite, cache_dir .. "/lushwal.vim" }
 )
 ]===])
-		fp:close()
-		vim.cmd("Shipwright " .. cache_path .. "/shipwright_build.lua")
-	else
-		error(
-			"Shipwright is required to compile this colorscheme. If you do not wish to compile, set vim.g.lushwal_configuration.compile_to_vimscript to false."
-		)
+			fp:close()
+			vim.cmd("Shipwright " .. cache_path .. "/shipwright_build.lua")
+		else
+			error(
+				"Shipwright is required to compile this colorscheme. If you do not wish to compile, set vim.g.lushwal_configuration.compile_to_vimscript to false."
+			)
+		end
 	end
 end
 
