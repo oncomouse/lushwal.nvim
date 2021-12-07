32969d29206738e53524b6c0df3c7a049dff6037
oncomouse <oncomouse@gmail.com>
2021-12-07 14:54:17 -0600

    Better Ale highlights

--- lua/lushwal/addons/ale.lua
+++ lua/lushwal/addons/ale.lua
@@ -4,8 +4,14 @@ local lush = require("lush")
 local colors = require("lushwal.colors")
 return lush(function()
 	return {
-		ALEErrorSign({ fg = colors.color5, bg = colors.background, gui = "bold" }),
-		ALEWarningSign({ fg = colors.color3, bg = colors.background, gui = "bold" }),
-		ALEInfoSign({ fg = colors.color8, bg = colors.background, gui = "bold" }),
+		ALEVirtualTextError({ fg = colors.color5, bg = colors.background }),
+		ALEVirtualTextWarning({ fg = colors.color3, bg = colors.background }),
+		ALEVirtualTextInfo({ fg = colors.color8, bg = colors.background }),
+		ALEError({ ALEVirtualTextError, gui = "underline" }),
+		ALEWarning({ ALEVirtualTextWarning, gui = "underline" }),
+		ALEInfo({ ALEVirtualTextInfo, gui = "underline" }),
+		ALEErrorSign({ ALEVirtualTextError, gui = "bold" }),
+		ALEWarningSign({ ALEVirtualTextWarning, gui = "bold" }),
+		ALEInfoSign({ ALEVirtualTextInfo, gui = "bold" }),
 	}
 end)
