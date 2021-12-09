f4cb9e3f827f389f552253b72aa7016b39e1d1ce
oncomouse <oncomouse@gmail.com>
2021-12-09 16:03:18 -0600

    What the what; error here that was REALLY hard to hunt down.

--- lua/lushwal/addons/gitsigns_nvim.lua
+++ lua/lushwal/addons/gitsigns_nvim.lua
@@ -3,7 +3,7 @@
 -- Sourced from: https://github.com/catppuccin/nvim
 local lush = require("lush")
 local colors = require("lushwal.colors")
-lush(function()
+return lush(function()
 	return {
 		GitSignsAdd({ fg = colors.color6 }),
 		GitSignsChange({ fg = colors.color3 }),
