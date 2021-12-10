b992690321431c76047e9352146f9da90ad48b6a
oncomouse <oncomouse@gmail.com>
2021-12-10 11:30:49 -0600

    Better colors for bar sections

--- lua/lushwal/addons/lualine.lua
+++ lua/lushwal/addons/lualine.lua
@@ -28,25 +28,22 @@ local command_fg = colors.background.hex
 
 -- Statusline:
 local statusline_active_fg = colors.color7.hex
-local statusline_active_bg = colors.color8.hex
+local statusline_active_bg = colors.background.hex
 local statusline_inactive_fg = colors.color7.hex
 local statusline_inactive_bg = colors.color8.hex
 
 return {
 	visual = {
 		a = { fg = visual_fg, bg = visual_bg, gui = "bold" },
-		b = { fg = visual_bg, bg = visual_fg },
-		c = { fg = statusline_active_fg, bg = statusline_active_bg },
+		b = { fg = visual_bg, colors.color8.hex },
 	},
 	replace = {
 		a = { fg = replace_fg, bg = replace_bg, gui = "bold" },
-		b = { fg = replace_bg, bg = replace_fg },
-		c = { fg = statusline_active_fg, bg = statusline_active_bg },
+		b = { fg = replace_bg, colors.color8.hex },
 	},
 	command = {
 		a = { fg = command_fg, bg = command_bg, gui = "bold" },
-		b = { fg = command_bg, bg = command_fg },
-		c = { fg = statusline_active_fg, bg = statusline_active_bg },
+		b = { fg = command_bg, colors.color8.hex },
 	},
 	inactive = {
 		a = { fg = statusline_inactive_fg, bg = statusline_inactive_bg, gui = "bold" },
@@ -55,12 +52,11 @@ return {
 	},
 	normal = {
 		a = { fg = normal_fg, bg = normal_bg, gui = "bold" },
-		b = { fg = normal_bg, bg = normal_fg },
+		b = { fg = normal_bg, colors.color8.hex },
 		c = { fg = statusline_active_fg, bg = statusline_active_bg },
 	},
 	insert = {
 		a = { fg = insert_fg, bg = insert_bg, gui = "bold" },
-		b = { fg = insert_bg, bg = insert_fg },
-		c = { fg = statusline_active_fg, bg = statusline_active_bg },
+		b = { fg = insert_bg, colors.color8.hex },
 	},
 }
