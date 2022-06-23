5f32b49b9475c193806432428d4fc7614a87e08d
oncomouse <oncomouse@gmail.com>
2022-06-23 13:59:19 -0500

    Explicitly set ctermfg values here.

--- lua/lushwal/addons/gina.lua
+++ lua/lushwal/addons/gina.lua
@@ -5,22 +5,22 @@ local colors = require("lushwal").colors
 return lush(function()
 	-- selene: allow(undefined_variable)
 	return {
-		AnsiColor0({ fg = colors.color0 }),
-		AnsiColor1({ fg = colors.color1 }),
-		AnsiColor2({ fg = colors.color2 }),
-		AnsiColor3({ fg = colors.color3 }),
-		AnsiColor4({ fg = colors.color4 }),
-		AnsiColor5({ fg = colors.color5 }),
-		AnsiColor6({ fg = colors.color6 }),
-		AnsiColor7({ fg = colors.color7 }),
-		AnsiColor8({ fg = colors.color8 }),
-		AnsiColor9({ fg = colors.color9 }),
-		AnsiColor10({ fg = colors.color10 }),
-		AnsiColor11({ fg = colors.color11 }),
-		AnsiColor12({ fg = colors.color12 }),
-		AnsiColor13({ fg = colors.color13 }),
-		AnsiColor14({ fg = colors.color14 }),
-		AnsiColor15({ fg = colors.color15 }),
+		AnsiColor0({ fg = colors.color, ctermfg = 0 }),
+		AnsiColor1({ fg = colors.color, ctermfg = 1 }),
+		AnsiColor2({ fg = colors.color, ctermfg = 2 }),
+		AnsiColor3({ fg = colors.color, ctermfg = 3 }),
+		AnsiColor4({ fg = colors.color, ctermfg = 4 }),
+		AnsiColor5({ fg = colors.color, ctermfg = 5 }),
+		AnsiColor6({ fg = colors.color, ctermfg = 6 }),
+		AnsiColor7({ fg = colors.color, ctermfg = 7 }),
+		AnsiColor8({ fg = colors.color, ctermfg = 8 }),
+		AnsiColor9({ fg = colors.color, ctermfg = 9 }),
+		AnsiColor10({ fg = colors.color, ctermfg = 10 }),
+		AnsiColor11({ fg = colors.color, ctermfg = 11 }),
+		AnsiColor12({ fg = colors.color, ctermfg = 12 }),
+		AnsiColor13({ fg = colors.color, ctermfg = 13 }),
+		AnsiColor14({ fg = colors.color, ctermfg = 14 }),
+		AnsiColor15({ fg = colors.color, ctermfg = 15 }),
 	}
 	-- selene: deny(undefined_variable)
 end)
