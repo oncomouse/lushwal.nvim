35ea3918f329219063933b929694b70edb10c45a
oncomouse <oncomouse@gmail.com>
2022-06-24 15:03:58 -0500

    Better plugin

--- plugin/lushwal-compile-if-stale.lua
+++
@@ -1,9 +0,0 @@
-local lushwal = require("lushwal")
-
-local autogrp = vim.api.nvim_create_augroup("lushwal", {})
-
-vim.api.nvim_create_autocmd("VimEnter", {
-	pattern = "*",
-	callback = lushwal.compile_if_stale,
-	group = autogrp,
-})
--- plugin/lushwal.lua
+++ plugin/lushwal.lua
@@ -1,2 +1,16 @@
 -- luacheck: globals vim
-vim.cmd("command! LushwalCompile lua require('lushwal.compile')()")
+local lushwal = require("lushwal")
+
+vim.api.nvim_create_user_command("LushwalCompile", function()
+	lushwal.compile()
+end, {})
+
+local autogrp = vim.api.nvim_create_augroup("lushwal", {})
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	pattern = "*",
+	callback = function()
+		lushwal.compile_if_stale()
+	end,
+	group = autogrp,
+})
