06b737a5e6d675634cd593cb356daff2707438c5
oncomouse <oncomouse@gmail.com>
2022-06-25 14:38:24 -0500

    Really complicated way to ensure that flocking actually works

--- lua/lushwal/watcher.lua
+++ lua/lushwal/watcher.lua
@@ -6,7 +6,18 @@ local function on_change(_, _, _)
 	w:stop()
 	vim.defer_fn(function()
 		local colorscheme_path = xdg("XDG_CONFIG_HOME") .. "/nvim/colors/lushwal.vim"
+		local stdout = vim.loop.new_pipe(false)
+		local stderr = vim.loop.new_pipe(false)
 		local handle
+		local locked = false
+		local function onread(err, data)
+			if err then
+				error(err)
+			end
+			if data then
+				locked = true
+			end
+		end
 		handle = vim.loop.spawn(
 			"perl",
 			{
@@ -14,23 +25,49 @@ local function on_change(_, _, _)
 					"-MFcntl=:flock",
 					"-e",
 					-- one-liner from: https://stackoverflow.com/a/13343904
-					[[$|=1; $f=shift; open(FH,$f) || die($!); flock(FH,LOCK_EX|LOCK_NB); system(join(" ",@ARGV)); flock(FH,LOCK_UN);]],
+					[[$|=1; $f=shift; open(FH,$f) || die($!); flock(FH,LOCK_EX|LOCK_NB) || die($!); system(join(" ",@ARGV)); flock(FH,LOCK_UN);]],
 					colorscheme_path,
 					[[nvim --headless +LushwalCompile +qall 2> /dev/null]],
 				},
-				stdio = { nil, nil, nil },
+				stdio = { nil, stdout, stderr },
 			},
 			vim.schedule_wrap(function()
+				stdout:read_stop()
+				stderr:read_stop()
+				stdout:close()
+				stderr:close()
 				handle:close()
-				require("lushwal").reload_theme()
+				if locked then
+					-- If flock has already triggered, we wait until compilation can finish:
+					vim.defer_fn(function()
+						require("lushwal").reload_theme()
+					end, 300)
+				else
+					-- Otherwise, run it right away:
+					require("lushwal").reload_theme()
+				end
 				watch_file()
 			end)
 		)
+		vim.loop.read_start(
+			stdout,
+			vim.schedule_wrap(function(...)
+				onread(...)
+			end)
+		)
+		vim.loop.read_start(
+			stderr,
+			vim.schedule_wrap(function(...)
+				onread(...)
+			end)
+		)
+
 		if handle == nil then
+			-- If there is no perl interpreter, we just compile and hope for hte best:
 			vim.notify("[Lushwal.nvim] Could not execute perl.")
 			require("lushwal").compile()
 		end
-	end, 100)
+	end, 1)
 end
 function watch_file()
 	local wal_path = xdg("XDG_CACHE_HOME") .. "/wal/colors.json"
